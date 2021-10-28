9f25a6bbd2dcd7a5af60ca4badd56d8809c43399
Maggie McCausland <maggiemccausland@hotmail.com>
2021-10-28 15:14:05 -0600

    want to read and read buttons function, append card item to appropriate div category, adjusted class names, removed commented out code

--- books.js
+++ books.js
@@ -2,48 +2,36 @@
 function getBooks() {
   document.querySelector("input").innerHTML = " ";
   document.querySelector("#results").innerHTML = " ";
-  document.querySelector(".total-number").innerHTML = "May take a minute to display results.";
+  document.querySelector(".total-number").innerHTML =
+    "May take a minute to display results.";
 
   fetch(
     "https://openlibrary.org/search.json?q=" +
       document.querySelector("input").value
   )
     .then((res) => res.json())
-    // .then((res) => {
-    //   for (let i = 0; i < res.docs.length; i++) {
-    //     document.querySelector("#results").innerHTML += `
-          // <div class="card" style="height: auto;">
-          // <div class="rating">
-          // <i class="rating__star far fa-star"></i>
-          // <i class="rating__star far fa-star"></i>
-          // <i class="rating__star far fa-star"></i>
-          // <i class="rating__star far fa-star"></i>
-          // <i class="rating__star far fa-star"></i>
-          // </div>
-          // <br>
-          // <button>Have Read</button>
-          // <button>Want to Read</button>
-          // </div>
-    //       `;
-    //   }
-    // })
     .then(async (data) => {
       data.docs.forEach((author) =>
-        addToList(author.title, author.author_name, author.first_publish_year, author.key,)
+        addToList(
+          author.title,
+          author.author_name,
+          author.first_publish_year,
+          author.key
+        )
       );
-      document.querySelector(".total-number").innerHTML = `Books found: ${data.docs.length}`;
-      console.log(data.docs)
+      // data.docs.forEach((book) => addWishlist(book.key));
+      document.querySelector(
+        ".total-number"
+      ).innerHTML = `Books found: ${data.docs.length}`;
       return data.docs[0];
     })
     .catch((error) => {
-      console.error(error)
+      console.error(error);
       document.querySelector(
         "#results"
       ).innerHTML += `<h3 style="margin: 2%;"> Result(s) not found.</h3>`;
     });
 }
-
-
 
 function addToList(bookTitle, authorName, bookYear, bookKey) {
   let content = document.querySelector("#results");
@@ -66,7 +54,6 @@ function addToList(bookTitle, authorName, bookYear, bookKey) {
   cardAuthor.className = "card-subtitle mb-2 card-author";
   cardAuthor.innerHTML = `Author(s): ${authorName}`;
 
-
   let yearDiv = document.createElement("div");
   let cardYearResult = document.createElement("h6");
   cardYearResult.className = "card-subtitle mb-2 muted-text card-year";
@@ -74,26 +61,35 @@ function addToList(bookTitle, authorName, bookYear, bookKey) {
 
   let buttonDiv = document.createElement("div");
 
-  let haveBtn = document.createElement('button');
+  let haveBtn = document.createElement("button");
   haveBtn.className = "have-read";
   haveBtn.innerHTML = "Have Read";
+  
+  haveBtn.addEventListener('click', () => {
+    let haveRead = document.querySelector(".readContent div")
+    haveRead.appendChild(haveBtn.parentElement.parentElement);
+  })
 
-  let wantBtn = document.createElement('button');
+  let wantBtn = document.createElement("button");
   wantBtn.className = "want-to-read";
   wantBtn.innerHTML = "Want to Read";
 
-//   let ratingDiv = document.createElement("div");
-//   let span = document.createElement("span");
-//   ratingDiv.className = "container";
-//   span.setAttribute("id", "rateMe1");
+  wantBtn.addEventListener('click', () => {
+    let wishlist = document.querySelector(".wishlistContent div");
+    wishlist.appendChild(wantBtn.parentElement.parentElement);
+  })
 
 
+  //   let ratingDiv = document.createElement("div");
+  //   let span = document.createElement("span");
+  //   ratingDiv.className = "container";
+  //   span.setAttribute("id", "rateMe1");
 
   // Appending
   cardBody.appendChild(ratingDiv);
   cardBody.appendChild(cardTitle);
   cardBody.appendChild(cardAuthor);
-  cardBody.appendChild(yearDiv)
+  cardBody.appendChild(yearDiv);
   yearDiv.appendChild(cardYearResult);
   cardBody.appendChild(buttonDiv);
   cardBody.appendChild(wantBtn);
@@ -104,8 +100,8 @@ function addToList(bookTitle, authorName, bookYear, bookKey) {
 
 
 // Toggling Sections
-function toggleToRead() {
-  let content = document.querySelector(".toReadContent");
+function toggleWishlist() {
+  let content = document.querySelector(".wishlistContent");
   if (content.style.display === "none") {
     content.style.display = "block";
   } else {
